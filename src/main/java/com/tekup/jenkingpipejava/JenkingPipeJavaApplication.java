8e84f55532fa66a14b371594db73e5e220ce34b7
hassendabb <dabbabi.hsan@gmail.com>
2024-01-30 15:14:20 +0100

    changes test 2

--- src/main/java/com/tekup/jenkingpipejava/JenkingPipeJavaApplication.java
+++ src/main/java/com/tekup/jenkingpipejava/JenkingPipeJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkingPipeJavaApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(JenkingPipeJavaApplication.class, args);
 	}
 
